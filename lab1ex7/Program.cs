cf42bbb1992ae3ceaa0076c95ebcb64e633e9de4
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 18:07:35 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //afisam par sau impar in functie de numar
+            int a;
+            Console.WriteLine("Introduceti un numar: ");
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0) Console.WriteLine("par");
+            else Console.WriteLine("impar");
+        }
+    }
+}
